ab27c4e6a7e77535edfb5ebb8694763b765c478d
shabana kauser <shabanakauser89@gmail.com>
2022-12-06 11:44:18 +0000

    delete specific task button styled

--- components/Routine/TaskDisplay.js
+++ components/Routine/TaskDisplay.js
@@ -3,7 +3,6 @@ import React from "react";
 import CheckBox from "@react-native-community/checkbox";
 import { useState } from "react";
 import CancelIcon from "@mui/icons-material/Cancel";
-import { red } from "@mui/material/colors";
 
 export default function TaskDisplay({ task, deleteTask, id }) {
   const [isClicked, setIsClicked] = useState(false);
@@ -21,16 +20,16 @@ export default function TaskDisplay({ task, deleteTask, id }) {
         <Text style={isClicked === true ? styles.isClickedText : styles.text}>
           {task}
         </Text>
-        <View style={styles.deleteTaskButton}>
-          <CancelIcon
-            style={{
-              fill: "#e21d3a",
-              height: "25px",
-              width: "25px",
-            }}
-            onClick={() => deleteTask(id)}
-          />
-        </View>
+      </View>
+      <View style={styles.deleteTaskButton}>
+        <CancelIcon
+          style={{
+            fill: "#a5a0ac",
+            height: "25px",
+            width: "25px",
+          }}
+          onClick={() => deleteTask(id)}
+        />
       </View>
     </View>
   );
@@ -39,12 +38,12 @@ export default function TaskDisplay({ task, deleteTask, id }) {
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
-    justifyContent: "",
-    alignContent: "center",
+    // justifyContent: "",
+    alignItems: "center",
     width: "20rem",
-    marginBottom: 1,
+    // marginBottom: 1,
     marginLeft: 5,
-    borderColor: "#77567A",
+    // borderColor: "#77567A",
     // borderWidth: "2px",
     // borderRadius: "10px",
   },
@@ -79,13 +78,12 @@ const styles = StyleSheet.create({
     textDecorationLine: "line-through",
   },
   deleteTaskButton: {
-    justifyContent: "space-between",
-    flexDirection: "row",
-    marginBottom: "10px",
+    // marginBottom: "10px",
     width: "30px",
+    height: "30px",
     // borderColor: "blue",
     // borderWidth: "2px",
-    marginTop: "10px",
-    marginRight: 10,
+
+    // marginRight: "10px",
   },
 });
